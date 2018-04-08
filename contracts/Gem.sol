705dacea4c37c76281c71cc9ac9bebc33bbf07f2
vgorin <vgrn.at@gmail.com>
2018-04-09 00:36:40 +0300

    implement important ERC721 methods: ownerOf, balanceOf, exists

--- contracts/Gem.sol
+++ contracts/Gem.sol
@@ -18,7 +18,7 @@ contract Gem {
 	mapping(uint256 => uint256) public tokens;
 
 	/// @notice total number of tokens owned by each each account
-	mapping(address => uint256) public balanceOf;
+	mapping(address => uint256) public balances;
 
 	/// @notice total number of tokens which exist in the system
 	uint256 public totalSupply;
@@ -47,6 +47,29 @@ contract Gem {
 		// grant the contract creator full permissions
 		operators[creator] = PERM_FULL;
 	}
+	
+	function ownerOf(uint256 tokenId) public constant returns (address) {
+		// get the token from storage
+		uint256 token = tokens[tokenId];
+
+		// check if this token exists
+		require(token > 0);
+
+		// extract token's address and return
+		return address(token);
+	}
+
+	function balanceOf(address who) public constant returns (uint256) {
+		// simply read the balance from balances
+		return balances[who];
+	}
+
+	function exists(uint256 tokenId) public constant returns (bool) {
+		// get the token from storage
+		uint256 token = tokens[tokenId];
+		// check if this token exists
+		return token > 0;
+	}
 
 	function updateFeatures(uint32 mask) public {
 		// call sender nicely - caller
@@ -64,4 +87,59 @@ contract Gem {
 		f &= PERM_FULL ^ (p & (PERM_FULL ^ mask));
 	}
 
+	function mint(uint256 id, address to) public {
+		// call sender nicely - caller
+		address caller = msg.sender;
+		// read caller's permissions
+		uint32 p = operators[caller];
+
+		// feature must be enabled globally and
+		// caller should have a permission to mint a token
+		require(f & p & PERM_MINT == PERM_MINT);
+		// the token specified should not already exist
+		require(tokens[id] == 0);
+
+		// init token value with new owner address
+		uint256 token = uint256(to);
+		// persist newly created token
+		tokens[id] = token;
+		// update token owner balance
+		balances[to]++;
+		// update total tokens number
+		totalSupply++;
+
+		// fire an event
+		Minted(id, to);
+	}
+
+	function burn(uint256 id) public {
+		// call sender nicely - caller
+		address caller = msg.sender;
+		// read caller's permissions
+		uint32 p = operators[caller];
+
+		// feature must be enabled globally and
+		// caller should have a permission to burn a token
+		require(f & p & PERM_BURN == PERM_BURN);
+
+		// get the token from storage
+		uint256 token = tokens[id];
+
+		// a token to burn should exist
+		require(token > 0);
+
+		// extract token owner address
+		address from = address(token);
+
+		// update token owner balance
+		balances[from]--;
+		// update total tokens number
+		totalSupply--;
+
+		// delete token
+		delete tokens[id];
+
+		// fire an event
+		Burnt(id, from);
+	}
 }
--- test/gem.js
+++ test/gem.js
@@ -17,5 +17,38 @@ contract('Gem', function(accounts) {
 		await gem.updateFeatures(0);
 		assert.equal(await gem.f(), 0, "creator cannot disable all the features");
 	});
+	it("initial state: no tokens exist initially", async function() {
+		const gem = await Gem.new();
+		assert.equal(await gem.totalSupply(), 0, "initial totalSupply is not zero");
+		assert.equal(await gem.balanceOf(accounts[0]), 0, "initial creator balance is not zero");
+	});
 
+	it("token creation routine: it is possible to mint a token", async function() {
+		const gem = await Gem.new();
+		assert.equal(await gem.totalSupply(), 0, "initial totalSupply is not zero");
+		assert.equal(await gem.balanceOf(accounts[0]), 0, "initial creator balance is not zero");
+		await gem.updateFeatures(0x00000008);
+		await gem.mint(0x1, accounts[0]);
+		assert.equal(await gem.exists(0x1), true, "newly created token doesn't exist");
+		assert.equal(await gem.totalSupply(), 1, "wrong total supply after creating first token");
+		assert.equal(await gem.balanceOf(accounts[0]), 1, "wrong creator balance after creating first token");
+		assert.equal(await gem.ownerOf(0x1), accounts[0], "wrong token owner after creating first token");
+	});
+
+	it("token destruction routine: it is possible to burn a token", async function() {
+		const gem = await Gem.new();
+		assert.equal(await gem.totalSupply(), 0, "initial totalSupply is not zero");
+		assert.equal(await gem.balanceOf(accounts[0]), 0, "initial creator balance is not zero");
+		await gem.updateFeatures(0x00000008);
+		await gem.mint(0x1, accounts[0]);
+		assert.equal(await gem.exists(0x1), true, "newly created token doesn't exist");
+		assert.equal(await gem.totalSupply(), 1, "wrong total supply after creating first token");
+		assert.equal(await gem.balanceOf(accounts[0]), 1, "wrong creator balance after creating first token");
+		assert.equal(await gem.ownerOf(0x1), accounts[0], "wrong token owner after creating first token");
+		await gem.updateFeatures(0x00000010);
+		await gem.burn(0x1);
+		assert.equal(await gem.exists(0x1), false, "newly created and destroyed token still exists");
+		assert.equal(await gem.totalSupply(), 0, "wrong total supply after creating and destroying first token");
+		assert.equal(await gem.balanceOf(accounts[0]), 0, "wrong creator balance after creating and destroying first token");
+	});
 });
