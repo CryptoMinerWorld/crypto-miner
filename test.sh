160965413c174ecf498e145eff26ed364c4defe0
vgorin <vgrn.at@gmail.com>
2018-06-14 00:29:39 +0300

    buy all 5500 geodes test

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # rm -rf build
-testrpc --gasPrice=20 --port=8666 --accounts=20 > /tmp/testrpc.log 2>&1 &
+ganache-cli --defaultBalanceEther=1000 --gasPrice=1 --gasLimit=0xffffffff --port=8666 --accounts=20 > /tmp/testrpc.log 2>&1 &
 truffle test --network=test
-kill -9 `ps -ef | grep 'port=8666' | grep testrpc | awk '{print \$2}'`
-
+kill $(lsof -t -i :8666)
---
+++ test/presale5500.js
@@ -0,0 +1,78 @@
+const ROLE_TOKEN_CREATOR = 0x00040000;
+const ROLE_ROLE_MANAGER = 0x10000000;
+
+const Token = artifacts.require("./GemERC721");
+const Sale = artifacts.require("./Presale");
+
+contract('Presale 5500', function(accounts) {
+	it("presale: buying all 5500 geodes", async function() {
+		const tk = await Token.new();
+		const sale = await Sale.new(tk.address, accounts[9]);
+		await tk.updateFeatures(ROLE_ROLE_MANAGER | ROLE_TOKEN_CREATOR);
+		await tk.addOperator(sale.address, ROLE_TOKEN_CREATOR);
+
+		const first500 = (await sale.currentPrice()).times(10);
+		const next5000 = first500.times(2);
+		process.stdout.write("    ");
+		for(let i = 0; i < 500; i++) {
+			process.stdout.write("" + i);
+			await sale.getGeodes.sendTransaction({
+				from: accounts[i % 8],
+				value: i < 46? first500: next5000
+			});
+			process.stdout.write(".");
+		}
+		process.stdout.write("\n");
+
+		let allTheGems = [];
+		for(let i = 0; i < 8; i++) {
+			const collection = await tk.getPackedCollection(accounts[i]);
+			console.log("    collection " + i + " size: " + collection.length);
+			allTheGems = allTheGems.concat(collection);
+		}
+
+		console.log("    total gems bought: " + allTheGems.length);
+		const colorNames = {
+			1: "January",
+			2: "February",
+			3: "March",
+			4: "April",
+			5: "May",
+			6: "June",
+			7: "July",
+			8: "August",
+			9: "September",
+			10: "October",
+			11: "Noveber",
+			12: "December",
+		};
+		const colors = {};
+		const levels = {};
+		const gradeNames = {
+			1: "Grade D",
+			2: "Grade C",
+			3: "Grade B",
+			4: "Grade A",
+			5: "Grade AA",
+			6: "Grade AAA",
+		};
+		const grades = {};
+		for(let i = 0; i < allTheGems.length; i++) {
+			const colorId = allTheGems[i].dividedToIntegerBy(0x1000000).modulo(0x100);
+			const levelId = allTheGems[i].dividedToIntegerBy(0x10000).modulo(0x100);
+			const gradeType = allTheGems[i].dividedToIntegerBy(0x100).modulo(0x100);
+			colors[colorId] = ++colors[colorId] || 0;
+			levels[levelId] = ++levels[levelId] || 0;
+			grades[gradeType] = ++grades[gradeType] || 0;
+		}
+		for(let i = 1; i <= 12; i++) {
+			console.log("    " + colorNames[i] + ": \t" + (colors[i] || 0));
+		}
+		for(let i = 1; i <= 5; i++) {
+			console.log("    Level " + i + ":\t" + (levels[i] || 0));
+		}
+		for(let i = 1; i <= 6; i++) {
+			console.log("    " + gradeNames[i] + ":\t" + (grades[i] || 0));
+		}
+	});
+});
