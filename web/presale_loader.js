6c8199a835b52e097b072d53d9952e117a4eb505
Sophophilia Studios <sophophiliastudios@gmail.com>
2018-06-21 14:48:36 -0400

    Small tweak to previous commit
    Added spaces around "-" in gem info text

--- web/presale_loader.js
+++ web/presale_loader.js
@@ -265,7 +265,7 @@ jQuery3(document).ready(function() {
 			return `
 				<a href="javascript:display_gem(\'` + gem.id + `\', \'` + color + `\', \'` + level + `\', \'` + grade + `\', \'` + miningRate + `\')">
 					<img style="padding: 0;" width="250" height="250" src="` + thumbnail + `"/>
-				</a><br/>Lvl ` + levelId + ` ` + color.substr(0, color.indexOf(" ")) + ` Grade ` + grade + `-` + miningRate + `%
+				</a><br/>Lvl ` + levelId + ` ` + color.substr(0, color.indexOf(" ")) + ` Grade ` + grade + ` - ` + miningRate + `%
 			`;
 		}
 
