ac8148ee54b7c1480c9b234392c07eadb0f8b9e4
Sophophilia Studios <sophophiliastudios@gmail.com>
2018-06-21 19:35:53 -0400

    trying a quick fix

--- web/presale_loader.js
+++ web/presale_loader.js
@@ -234,7 +234,7 @@ jQuery3(document).ready(function() {
 		const columns = 4;
 		const rows = Math.ceil(collection.length / columns);
 		let html = "";
-		html += '<h1 id="my_geodes_header">' + collection.length + ' Gems And id="my_geodes_subheader" in your collection!</h1>';
+		html += '<h1 id="my_geodes_header">' + collection.length + ' Gems And </h1>' + '<h1 id="my_geodes_subheader"> in your collection!</h1>';
 
 		html += `
 			<div id="gem_sorting_options">
