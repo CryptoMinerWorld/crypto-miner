b9e1f57f7ef6bebf263a139597af45f5fc65c62c
Sophophilia Studios <sophophiliastudios@gmail.com>
2018-06-21 20:16:53 -0400

    Simplifying header info

--- web/presale_loader.js
+++ web/presale_loader.js
@@ -234,8 +234,8 @@ jQuery3(document).ready(function() {
 		const columns = 4;
 		const rows = Math.ceil(collection.length / columns);
 		let html = "";
-		html += '<h1 id="my_geodes_header" style="display:inline;">' + collection.length + ' Gems And </h1>' + '<h1 id="my_geodes_subheader" style="display:inline;"> in your collection!</h1>';
-		html += `</br>`;
+		html += '<h2 id="my_geodes_header">' + collection.length + ' Gemstone Workers</h2>';
+		html += '<h2 id="my_geodes_subheader"></h2>';
 		html += `
 			<div id="gem_sorting_options">
 				<select id="gem_sorting_by">
@@ -399,7 +399,7 @@ jQuery3(document).ready(function() {
 							if(err || err > 0) {
 								return;
 							}
-							if(result.length<=1){
+							if(result.length>=1){
 								jQuery3("#my_geodes_subheader").html(result + " Founders Plot of Land");
 							}
 							else{
