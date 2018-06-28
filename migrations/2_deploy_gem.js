94ea416bc81416410661a67bdc011c0a08ff1398
vgorin <vgrn.at@gmail.com>
2018-06-29 00:19:27 +0300

    update deployment scripts and deployed instances

--- migrations/2_deploy_gem.js
+++ migrations/2_deploy_gem.js
@@ -1,4 +1,10 @@
 const ROLE_TOKEN_CREATOR = 0x00040000;
+const ROLE_COUPON_MANAGER = 0x00000100;
+
+const Gem = artifacts.require("./GemERC721");
+const Sale = artifacts.require("./CouponSale");
+const AddressUtils = artifacts.require("./AddressUtils");
+const StringUtils = artifacts.require("./StringUtils");
 
 module.exports = async function(deployer, network, accounts) {
 	if(network === "test") {
@@ -10,10 +16,10 @@ module.exports = async function(deployer, network, accounts) {
 		return;
 	}
 
-	const Gem = artifacts.require("./GemERC721");
-	const Sale = artifacts.require("./CouponSale");
-	const AddressUtils = artifacts.require("./AddressUtils");
-	const StringUtils = artifacts.require("./StringUtils");
+	let beneficiary = "0x92589b179c2e902770140011367b286c1741dd63"; // MainNet MultiSig
+	if(network === "development") {
+		beneficiary = "0xb4e8e4f7e6024b37ed2584e8c86b2917dae9a2da"; // Rinkeby MultiSig
+	}
 
 	await deployer.deploy(AddressUtils);
 	await deployer.link(AddressUtils, Gem);
@@ -26,10 +32,13 @@ module.exports = async function(deployer, network, accounts) {
 	const gem = await Gem.deployed();
 	await gem.updateFeatures(0xFFFFFFFF);
 
-	await deployer.deploy(Sale, gem.address, accounts[0]);
+	await deployer.deploy(Sale, gem.address, beneficiary);
 
 	const sale = await Sale.deployed();
 	await gem.addOperator(sale.address, ROLE_TOKEN_CREATOR);
+
+	await sale.addOperator("0x501E13C2aE8D9232B88F63E87DFA1dF28103aCb6", ROLE_COUPON_MANAGER); // John
+	await sale.addOperator("0x391B9254BE4F56dadF57905113Dfb88a3bf8f141", ROLE_COUPON_MANAGER); // Myke
 
 	console.log("___________________________________________________");
 	console.log("gem:        " + gem.address);
