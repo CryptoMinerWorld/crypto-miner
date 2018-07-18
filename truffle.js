0d352b90c3a54508df483ad34083b0746ef0dee5
vgorin <vgrn.at@gmail.com>
2018-07-18 19:31:10 +0300

    update truffle.js to work with mainnet/infura setup

--- truffle.js
+++ truffle.js
@@ -1,16 +1,40 @@
-// npm install truffle-hdwallet-provider
+// npm install truffle-hdwallet-provider@0.0.3
 const HDWalletProvider = require("truffle-hdwallet-provider");
+
+// https://ethereum.stackexchange.com/questions/44349/truffle-infura-on-mainnet-nonce-too-low-error
+const NonceTrackerSubprovider = require("web3-provider-engine/subproviders/nonce-tracker");
 
 module.exports = {
 	networks: {
+		mainnet: {
+			provider: function () {
+				const wallet = new HDWalletProvider(
+					"***12 words***",
+					"https://rinkeby.infura.io/***key***"
+				);
+				const nonceTracker = new NonceTrackerSubprovider();
+				wallet.engine._providers.unshift(nonceTracker);
+				nonceTracker.setEngine(wallet.engine);
+				return wallet
+			},
+			network_id: "1", // Match mainnet only
+			gas: 4500000,
+			gasPrice: 11000000000 // 11 GWei
+		},
 		development: {
-			provider: new HDWalletProvider(
-				"12 mnemonic words",
-				"https://rinkeby.infura.io/***key***"
-			),
+			provider: function () {
+				const wallet = new HDWalletProvider(
+					"***12 words***",
+					"https://rinkeby.infura.io/***key***"
+				);
+				const nonceTracker = new NonceTrackerSubprovider();
+				wallet.engine._providers.unshift(nonceTracker);
+				nonceTracker.setEngine(wallet.engine);
+				return wallet
+			},
 			network_id: "*", // Match any network (determined by provider)
-			gas: 4700000,
-			gasPrice: 20000000000 // 20 GWei
+			gas: 4500000,
+			gasPrice: 21000000000 // 21 GWei
 		},
 		coverage: {
 			host: "localhost",
@@ -18,14 +42,22 @@ module.exports = {
 			port: 8555,
 			gas: 0xfffffffffff,
 			gasPrice: 0x01
+		},
+		test: {
+			host: "localhost",
+			network_id: "*",
+			port: 8666,
+			gas: 0xffffffff,
+			gasPrice: 1
 		}
 	},
-	/*
-		solc: {
-			optimizer: {
-				enabled: true,
-				runs: 200 // default is 200, however for function execution the effect is noticeable up to 20000
-			}
+	mocha: {
+		enableTimeouts: false
+	},
+	solc: {
+		optimizer: {
+			enabled: true,
+			runs: 200 // default is 200, however for function execution the effect is noticeable up to 20000
 		}
-	*/
+	}
 };
