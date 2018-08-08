7f9dd5c49160a61ba7adda1906a557d33935bb76
vgorin <vgrn.at@gmail.com>
2018-08-09 00:22:18 +0300

    referrer modal improvements

--- web/presale2_loader.js
+++ web/presale2_loader.js
@@ -141,7 +141,8 @@ document.write(`
 				<tr><td>10 per Gem</td><td>20 per Geode</td></tr>
 				<tr><td><input id="points_gems" type="number" min="10" max="100" step="10" value="0"/></td>
 				<td><input id="points_geodes" type="number" min="20" max="200" step="20" value="0"/></td></tr>
-				<tr><td colspan="2"><input type="button" value="Buy" onclick="" style="width: 100%;"/></td></tr>
+				<tr><td colspan="2"><span id="points_selected">0</span> of <span id="points_available">10</span></td></tr>
+				<tr><td colspan="2"><input type="button" value="Buy" onclick="" disabled/></td></tr>
 			</table>
 		</div>
 	</div>
