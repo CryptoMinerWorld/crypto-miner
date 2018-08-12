ab1818c0da0f006ab3b30b47789b50d534b4092b
vgorin <vgrn.at@gmail.com>
2018-08-12 22:34:59 +0300

    delayed launch smart contract modifications and tests

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 # rm -rf build
 ganache-cli --defaultBalanceEther=1000 --gasPrice=1 --gasLimit=0xffffffff --port=8666 --accounts=20 > /tmp/testrpc.log 2>&1 &
-truffle test --network=test
+truffle test --network=test $1
 kill $(lsof -t -i :8666)
