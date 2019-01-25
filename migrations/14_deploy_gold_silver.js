c26af1417ed4b4f2fafd51c48b802db742dc7ff1
vgorin <vgrn.at@gmail.com>
2019-01-25 18:18:53 +0200

    gold/silver deployment script improvements

--- migrations/14_deploy_gold_silver.js
+++ migrations/14_deploy_gold_silver.js
@@ -1,3 +1,10 @@
+// Enables ERC20 transfers of the tokens
+const FEATURE_TRANSFERS = 0x00000001;
+
+// Enables ERC20 transfers on behalf
+const FEATURE_TRANSFERS_ON_BEHALF = 0x00000002;
+
+// Gold/Silver tokens
 const GoldERC20 = artifacts.require("./GoldERC20");
 const SilverERC20 = artifacts.require("./SilverERC20");
 
@@ -21,7 +28,28 @@ module.exports = async function(deployer, network, accounts) {
 	const silver = await SilverERC20.deployed();
 	const silverAddress = silver.address;
 
+	// for test network:
+	if(network !== "mainnet") {
+		// enable transfers and transfers on behalf
+		await gold.updateFeatures(FEATURE_TRANSFERS | FEATURE_TRANSFERS_ON_BEHALF);
+		await silver.updateFeatures(FEATURE_TRANSFERS | FEATURE_TRANSFERS_ON_BEHALF);
+
+		// transfer some test amounts of gold and silver to test addresses
+		await gold.mint("0x501E13C2aE8D9232B88F63E87DFA1dF28103aCb6", 500); // John
+		await silver.mint("0x501E13C2aE8D9232B88F63E87DFA1dF28103aCb6", 5500);
+		await gold.mint("0xEE169DCC689D0C358F68Ce95DEf41646039aC190", 500); // Roman
+		await silver.mint("0xEE169DCC689D0C358F68Ce95DEf41646039aC190", 5500);
+		await gold.mint("0xEd6003e7A6494Db4ABabEB7bDf994A3951ac6e69", 500); // Basil
+		await silver.mint("0xEd6003e7A6494Db4ABabEB7bDf994A3951ac6e69", 5500);
+	}
+
+	// total supply values (for mainnet should be zero)
+	const goldSupply = await gold.totalSupply();
+	const silverSupply = await silver.totalSupply();
+
 	console.log("______________________________________________________");
-	console.log("gold:       " + goldAddress);
-	console.log("silver:     " + silverAddress);
+	console.log("gold:          " + goldAddress);
+	console.log("silver:        " + silverAddress);
+	console.log("gold supply:   " + goldSupply.toString(10));
+	console.log("silver supply: " + silverSupply.toString(10));
 };
