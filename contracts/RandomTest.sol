1f4ace46d4cc1296c8aaf53084e21f1f9af5105e
vgorin <vgrn.at@gmail.com>
2019-01-27 18:59:15 +0200

    non-uniform random related tests

---
+++ contracts/RandomTest.sol
@@ -0,0 +1,71 @@
+pragma solidity 0.4.23;
+
+import "./Random.sol";
+
+/**
+ * @title Random Library Test
+ *
+ * @dev A proxy smart contract to test Random Library
+ *
+ * @author basil Gorin
+ */
+contract RandomTest {
+  /**
+   * see Random.__rndVal
+   */
+  function rndVal(uint256 randomness, uint256 mask, uint64 offset, uint64 length) public pure returns (uint256) {
+    // delegate call to `Random.__rndVal` and return
+    return Random.__rndVal(randomness, mask, offset, length);
+  }
+
+  /**
+   * see Random.__randomValue
+   */
+  function randomValue(uint64 offset, uint64 length) public constant returns (uint256) {
+    // delegate call to `Random.__randomValue` and return
+    return Random.__randomValue(0, offset, length);
+  }
+
+  /**
+   * see Random.__quadraticRandom
+   */
+  function quadraticRandom(uint64 offset, uint64 length) internal constant returns (uint256) {
+    // delegate call to `Random.__quadraticRandom` and return
+    return Random.__quadraticRandom(0, offset, length);
+  }
+
+  /**
+   * see Random.__rawRandom
+   */
+  function rawRandom() internal constant returns (uint256) {
+    // delegate call to `Random.__rawRandom` and return
+    return Random.__rawRandom(0);
+  }
+
+  /**
+   * @notice Number of different grade values defined for a gem
+   * @dev Gem grade value is reassigned each time grade type increases,
+   *      grade value is generated as non-uniform random in range [0, GRADE_VALUES)
+   */
+  uint24 public constant GRADE_VALUES = 1000000;
+
+  /**
+   * @dev A function to check `Workshop.randomGradeValue` generation,
+   *      see Workshop.randomGradeValue
+   * @param n how many grade values to generate
+   * @return an array of grade values of length n
+   */
+  function randomGradeValues(uint32 n) public constant returns(uint24[]) {
+    // declare a container to store all the generated grades
+    uint24[] memory result = new uint24[](n);
+
+    // generate amount of random grade values requested
+    for(uint32 i = 0; i < n; i++) {
+      // generate random using exactly the same logic as in Workshop.randomGradeValue
+      result[i] = uint24(Random.__quadraticRandom(i, 0, GRADE_VALUES));
+    }
+
+    // return the result
+    return result;
+  }
+}
---
+++ test/workshop_rnd.js
@@ -0,0 +1,84 @@
+/*
+ * Test depth defines how many random values will be generated
+ * and analyzed to verify grade value random properties
+ * Number of randoms to be generated is equal to 2^TEST_DEPTH
+ */
+const TEST_DEPTH = 11;
+
+// GemERC721 smart contract
+const Gem = artifacts.require("./GemERC721.sol");
+// Silver smart contract
+const Silver = artifacts.require("./SilverERC20.sol");
+// GoldERC20 smart contract
+const Gold = artifacts.require("./GoldERC20.sol");
+// Workshop smart contract
+const Workshop = artifacts.require("./Workshop.sol");
+
+// Random Test smart contract helper
+const RandomTest = artifacts.require("./RandomTest.sol");
+
+// A test to check properties of the generated grade values random
+contract('Workshop (Grade Value Randomness)', (accounts) => {
+	it("random: ensure RandomTest and Workshop maximum grade value matches", async() => {
+		// construct workshop dependencies
+		const gem = await Gem.new();
+		const silver = await Silver.new();
+		const gold = await Gold.new();
+
+		// construct workshop itself
+		const workshop = await Workshop.new(gem.address, silver.address, gold.address);
+
+		// construct random test helper smart contract
+		const rndTest = await RandomTest.new();
+
+		// verify GRADE_VALUES constant is the same in both smart contracts
+		assert((await rndTest.GRADE_VALUES()).eq(await workshop.GRADE_VALUES()), "GRADE_VALUES mismatch");
+	});
+	it("random: verify properties required", async() => {
+		// construct random test helper smart contract
+		const rndTest = await RandomTest.new();
+
+		// number of random values to generate
+		const n = Math.pow(2, TEST_DEPTH);
+
+		// maximum possible grade value
+		const maxGrade = (await rndTest.GRADE_VALUES()).toNumber();
+
+		// generate some amount of random grade values
+		const gradeValues = await rndTest.randomGradeValues(n);
+
+		// sort the array
+		gradeValues.sort((a, b) => a.minus(b).toNumber());
+
+		// calculate the minimum
+		const min = gradeValues[0].toNumber();
+
+		// calculate the maximum
+		const max = gradeValues[gradeValues.length - 1].toNumber();
+
+		// calculate an average
+		const avg = gradeValues.reduce((a, b) => a.plus(b), web3.toBigNumber(0)).dividedToIntegerBy(gradeValues.length).toNumber();
+
+		// calculate the median
+		const median = gradeValues[Math.floor(gradeValues.length / 2)].toNumber();
+
+		// print the values into the console
+		console.log("\tmin: %o\n\tmax: %o\n\tavg: %o\n\tmed: %o", min, max, avg, median);
+
+		// verify the distribution properties are correct
+		assertEqual(0, min, maxGrade, "wrong minimum");
+		assertEqual(maxGrade, max, maxGrade, "wrong maximum");
+		assertEqual(maxGrade / 4, avg, maxGrade, "wrong average");
+	});
+});
+
+
+// asserts equal with precision of 5% of the maximum (absolute value)
+function assertEqual(expected, actual, maximum, msg) {
+	assertEqualWith(expected, maximum * 0.05, actual, msg);
+}
+
+// asserts equal with the precisions defined in leeway (absolute value)
+function assertEqualWith(expected, leeway, actual, msg) {
+	assert(expected - leeway < actual && expected + leeway > actual, msg);
+}
