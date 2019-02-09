8cbe2f2755e70e08f6c748ff1261c1cf0526e5c8
vgorin <vgrn.at@gmail.com>
2019-02-09 06:05:47 +0200

    dutch auction ABI, deployment script, deployed instance address

---
+++ migrations/17_deploy_auction_helper.js
@@ -0,0 +1,24 @@
+// Dutch Auction Helper
+const Helper = artifacts.require("./DutchAuctionHelper");
+
+module.exports = async function(deployer, network, accounts) {
+	if(network === "test") {
+		console.log("[deploy auction helper] test network - skipping the migration script");
+		return;
+	}
+	if(network === "coverage") {
+		console.log("[deploy auction helper] coverage network - skipping the migration script");
+		return;
+	}
+
+
+	// deploy auction helper sale
+	await deployer.deploy(Helper);
+
+	// get deployed instance
+	const helper = await Helper.deployed();
+
+	// deployment successful, print helper address
+	console.log("______________________________________________________");
+	console.log("auction helper: " + helper.address);
+};
