27b24edba660f2d40e6eea08c4f35f240197ad45
vgorin <vgrn.at@gmail.com>
2019-02-11 19:31:45 +0200

    fixes for solidity-coverage

--- .solcover.js
+++ .solcover.js
@@ -1,9 +1,4 @@
 // https://www.npmjs.com/package/solidity-coverage
 module.exports = {
-	accounts: 20,
-	testrpcOptions: "--defaultBalanceEther=1000 --defaultBalanceEther=1000 --gasPrice=1 --gasLimit=0xfffffffffff",
-/*
-	testCommand: 'ganache-cli --defaultBalanceEther=1000 --gasPrice=1 --gasLimit=0xfffffffffff --port=8555 --accounts=20 > /tmp/testrpc.log 2>&1 & truffle test --network=coverage $1',
-	norpc: true,
-*/
+	testrpcOptions: "--port 8555 --accounts 35 --defaultBalanceEther 1000"
 };
