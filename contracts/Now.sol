3eee90fcad46fe73599f0423612f235335e48ead
vgorin <vgrn.at@gmail.com>
2019-02-21 18:19:14 +0200

    deploy helper smart contract to verify sale start time

---
+++ contracts/Now.sol
@@ -0,0 +1,19 @@
+pragma solidity 0.4.23;
+
+/**
+ * @title Now
+ *
+ * @notice Helper smart contract, provides current unix timestamp
+ *
+ * @author Basil Gorin
+ */
+contract Now {
+  /**
+   * @notice provides current unix timestamp
+   * @return current time as a unix timestamp
+   */
+  function getNow() public constant returns(uint256) {
+    // just return the built-in constant
+    return now;
+  }
+}
