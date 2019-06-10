78d9ee4ed13dc5eef40d35285d6f1fdc18d693d1
vgorin <vgrn.at@gmail.com>
2019-06-10 04:29:58 +0300

    update gas prices

--- truffle-config.js
+++ truffle-config.js
@@ -53,8 +53,8 @@ module.exports = {
 				return wallet;
 			},
 			network_id: "4", // Rinkeby
-			gas: 7000000,
-			gasPrice: 7000000000, // 7 GWei
+			gas: 6500000,
+			gasPrice: 2000000000, // 2 GWei
 			skipDryRun: true, // if you don't want to test run the migration locally before the actual migration (default is false)
 			// timeoutBlocks: 500, // if a transaction is not mined, keep waiting for this number of blocks (default is 50)
 		},
@@ -76,7 +76,7 @@ module.exports = {
 			},
 			network_id: "3", // Ropsten
 			gas: 7500000,
-			gasPrice: 7000000000, // 7 GWei
+			gasPrice: 2000000000, // 2 GWei
 			skipDryRun: true, // if you don't want to test run the migration locally before the actual migration (default is false)
 			// timeoutBlocks: 500, // if a transaction is not mined, keep waiting for this number of blocks (default is 50)
 		},
