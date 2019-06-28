7cd2f305e661a387c59ed63a1dee9d7ba3785976
vgorin <vgrn.at@gmail.com>
2019-06-28 07:35:50 +0300

    skip dry run

--- truffle-config.js
+++ truffle-config.js
@@ -32,8 +32,8 @@ module.exports = {
 			},
 			network_id: "1", // Mainnet
 			gas: 7500000,
-			gasPrice: 1000000000, // 1 GWei
-			// skipDryRun: true, // if you don't want to test run the migration locally before the actual migration (default is false)
+			gasPrice: 2000000000, // 2 GWei
+			skipDryRun: true, // if you don't want to test run the migration locally before the actual migration (default is false)
 			timeoutBlocks: 500, // if a transaction is not mined, keep waiting for this number of blocks (default is 50)
 		},
 
